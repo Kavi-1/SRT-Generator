18a457c97a2c7774aa98d1df5b0eddc2b094d6f7
Kavi Lu <kavilu127@gmail.com>
2025-10-06 14:44:07 -0400

    allow request from vercel url

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "*"],
+    allow_origins=["http://localhost:5173", 
+                   "http://127.0.0.1:5173", 
+                   "https://srt-generator-two.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
